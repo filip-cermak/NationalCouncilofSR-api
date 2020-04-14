5e266e1b73d609a9069acd5a0a7f123c1cc0bf00
filip-cermak <fc469@cam.ac.uk>
2020-04-14 19:22:55 +0200

    server side cache

--- main.go
+++ main.go
@@ -3,22 +3,29 @@ package main
 // [START import]
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+
 	"github.com/gorilla/mux"
 	"gocloud.dev/server"
-	"strconv"
-	"io/ioutil"
-	)
+)
+
+var storage *Storage
+
+func init() {
+	storage = NewStorage()
+}
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", indexHandlerWebsite)
-	r.HandleFunc("/meetings/", indexHandlerMeetings)
-	r.HandleFunc("/voting/{id:[0-9]+}", indexHandlerVoting)
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandlerWebsite)
+	r.HandleFunc("/meetings/", cached("1d", indexHandlerMeetings))
+	r.HandleFunc("/voting/{id:[0-9]+}", cached("1d", indexHandlerVoting))
 
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
@@ -28,39 +35,39 @@ func main() {
 
 }
 
-func indexHandlerWebsite(w http.ResponseWriter, r *http.Request){
+func indexHandlerWebsite(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
 	content, err := ioutil.ReadFile("index.html")
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Fprint(w, string(content))
 
 }
 
-func indexHandlerMeetings(w http.ResponseWriter, r *http.Request){
+func indexHandlerMeetings(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 	fmt.Fprint(w, string(scrape_meeting_id()))
 }
 
-func indexHandlerVoting(w http.ResponseWriter, r *http.Request){
+func indexHandlerVoting(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 	vars := mux.Vars(r)
 	varID := vars["id"]
 	//fmt.Fprint(w, varId)
-	i,err := strconv.Atoi(varID)
+	i, err := strconv.Atoi(varID)
 
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprint(w,string(scrape_meeting(i)))
+	fmt.Fprint(w, string(scrape_meeting(i)))
 }
 
 func enableCors(w *http.ResponseWriter) {
